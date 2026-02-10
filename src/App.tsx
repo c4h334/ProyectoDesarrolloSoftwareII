5f21d413bd5d6f7db7de30ca5da5fa411e826fad
Evelyn <evemh19@gmail.com>
2026-02-10 01:34:05 -0600

    Se corrigieron las rutas y se da nota de que el layout evita la aparición del contenido por lo cual se quito de App.tsx (no contiene los cambios del footer realizados por un compañero)

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Layout from "./components/layout/layout.tsx";
+import Header from "./shared/Header.tsx";
+import Footer from "./shared/Footer.tsx";
 
 import Home from "./features/home/Home";
+import ClientProfile from "./features/client/ClientProfile";
 
 function App() {
   return (
     <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Layout>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-1">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/client-profile" element={<ClientProfile />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
     </Router>
   );
 }
